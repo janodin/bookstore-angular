f563eddf2aa3d676a9e150f4aea2268310b159ae
Janodin Patundog <105004639+janodin@users.noreply.github.com>
2022-06-04 02:51:05 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://willowy-chaja-417005.netlify.app/';
+const baseUrl = 'https://willowy-chaja-417005.netlify.app';
 
 export const environment = {
   production: false,
@@ -10,7 +10,7 @@ export const environment = {
     logoUrl: '',
   },
   oAuthConfig: {
-    issuer: 'http://janopatundog-001-site1.btempurl.com',
+    issuer: 'http://janopatundog-001-site1.btempurl.com/',
     redirectUri: baseUrl,
     clientId: 'BookStore_App',
     responseType: 'code',
@@ -19,7 +19,7 @@ export const environment = {
   },
   apis: {
     default: {
-      url: 'http://janopatundog-001-site1.btempurl.com',
+      url: 'http://janopatundog-001-site1.btempurl.com/',
       rootNamespace: 'Acme.BookStore',
     },
   },
