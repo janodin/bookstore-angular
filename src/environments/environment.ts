7989f2b54126ddc06bedbffb941c80451d23dbb8
Janodin Patundog <105004639+janodin@users.noreply.github.com>
2022-06-04 18:18:39 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://willowy-chaja-417005.netlify.app';
+const baseUrl = 'https://jano-bookstore.netlify.app';
 
 export const environment = {
   production: false,
