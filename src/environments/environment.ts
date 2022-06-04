60958bdfc71808a94a087770cc9c5d7f8cdc0ac8
Janodin Patundog <105004639+janodin@users.noreply.github.com>
2022-06-04 12:29:11 +0800

    Create environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '@abp/ng.core';
 
-const baseUrl = 'https://willowy-chaja-417005.netlify.app';
+const baseUrl = 'http://localhost:4200';
 
 export const environment = {
   production: false,
